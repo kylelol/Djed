62225cc850fa2d80020f10a808c83f49572033f1
Kyle Kirkland <Kyle@Kyles-iMac.local>
2015-07-01 20:57:55 -0400

    Initial Commit

---
+++ Djed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Djed
+//
+//  Created by Kyle Kirkland on 7/1/15.
+//  Copyright (c) 2015 Kyle Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
